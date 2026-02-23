078c04d79e07bc5fdd81f44ad71d4e8b3f227f3b
uusrajaminyak <yustinushendisetyawan@gmail.com>
2026-02-23 15:47:02 +0700

    Pertemuan02 selesai

--- Pertemuan02/src/MTitik.java
+++ Pertemuan02/src/MTitik.java
@@ -17,5 +17,28 @@ public class MTitik {
         T1.setAbsis(10);
         T1.setOrdinat(10);
         T2.printTitik();
+
+        Titik T3 = new Titik();
+        Titik T4 = new Titik(3, 5);
+
+        System.out.println("Jumlah objek Titik yang telah dibuat: " + Titik.getCounterTitik());
+        // System.out.println("Jumlah objek Titik yang telah dibuat: " + T4.getCounterTitik());
+
+        T3.getKuadran();
+        T4.getKuadran();
+
+        T3.getJarakPusat();
+        T4.getJarakPusat();
+
+        T3.getJarak(T4);
+        T4.getJarak(T3);
+
+        T3.refleksiX();
+        T3.printTitik();
+        T3.getRefleksiX();
+
+        T4.refleksiY();
+        T4.printTitik();
+        T4.getRefleksiY();
     }
 }
--- Pertemuan02/src/Titik.java
+++ Pertemuan02/src/Titik.java
@@ -7,11 +7,21 @@ public class Titik {
     // Atribut
     double absis;
     double ordinat;
+    static int counterTitik = 0;
 
     // Konstruktor
+    Titik(double x, double y) {
+        absis = x;
+        ordinat = y;
+        counterTitik++;
+    }
+
     Titik() {
-        absis = 0;
-        ordinat = 0;
+        this(0, 0);
+    }
+
+    static int getCounterTitik() {
+        return counterTitik;
     }
 
     double getAbsis() {
@@ -37,5 +47,53 @@ public class Titik {
 
     void printTitik() {
         System.out.println("Titik(" + absis + ", " + ordinat + ")");
+    }
+
+    // void printCounterTitik() {
+    //     System.out.println(this.counterTitik);
+    // }
+
+    void getKuadran() {
+        if (absis > 0 && ordinat > 0) {
+            System.out.println("Titik berada pada kuadran I");
+        } else if (absis < 0 && ordinat > 0) {
+            System.out.println("Titik berada pada kuadran II");
+        } else if (absis < 0 && ordinat < 0) {
+            System.out.println("Titik berada pada kuadran III");
+        } else if (absis > 0 && ordinat < 0) {
+            System.out.println("Titik berada pada kuadran IV");
+        } else if (absis == 0 && ordinat == 0) {
+            System.out.println("Titik berada pada titik asal");
+        } else if (absis == 0) {
+            System.out.println("Titik berada pada sumbu Y");
+        } else if (ordinat == 0) {
+            System.out.println("Titik berada pada sumbu X");
+        }
+    }
+
+    void getJarakPusat() {
+        double jarak = Math.sqrt(absis * absis + ordinat * ordinat);
+        System.out.println("Jarak titik ke pusat: " + jarak);
+    }
+
+    void getJarak(Titik T) {
+        double jarak = Math.sqrt((absis - T.absis) * (absis - T.absis) + (ordinat - T.ordinat) * (ordinat - T.ordinat));
+        System.out.println("Jarak titik ke titik lain: " + jarak);
+    }
+
+    void refleksiX() {
+        ordinat = -ordinat;
+    }
+
+    void refleksiY() {
+        absis = -absis;
+    }
+
+    void getRefleksiX() {
+        System.out.println("Refleksi terhadap sumbu X: Titik(" + absis + ", " + (-ordinat) + ")");
+    }
+
+    void getRefleksiY() {
+        System.out.println("Refleksi terhadap sumbu Y: Titik(" + (-absis) + ", " + ordinat + ")");
     }
 }
